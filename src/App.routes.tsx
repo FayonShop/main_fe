44e84deb6519ed703098615e69430e1d4371a417
Sviatoslav <s.kytsara@gmail.com>
2024-11-09 23:22:43 +0200

    feat: setup routing, create custom navlink, create public and public routes

---
+++ src/App.routes.tsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from "react-router-dom";
+import BasicLayout from "./layouts/BasicLayout/BasicLayout";
+import ContentPage from "./pages/ContentPage/ContentPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<BasicLayout/>}>
+    <Route index element={<ContentPage/>}/>
+
+    <Route path="*" element={<NotFoundPage/>}/>
+  </Route>
+));
+
+export default function AppRoutes() {
+  return <RouterProvider router={router}/>;
+}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
+import styles from "./Sidebar.module.scss";
+import CNavLink from "../ui/CNavLink/CNavLink";
+
 export default function Sidebar() {
   return (
-    <div>
-
+    <div className={styles.container}>
+      <CNavLink to="/">Main</CNavLink>
+      <CNavLink to="/test">Test</CNavLink>
     </div>
   );
 }
