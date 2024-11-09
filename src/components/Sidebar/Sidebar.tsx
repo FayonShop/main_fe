b44b4f43827dc5c7754398d99eb68d4be3625165
Sviatoslav <s.kytsara@gmail.com>
2024-11-09 23:26:08 +0200

    fix: remove old BrowserRouter

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,9 @@
 import styles from "./Sidebar.module.scss";
-import CNavLink from "../ui/CNavLink/CNavLink";
 
 export default function Sidebar() {
   return (
     <div className={styles.container}>
-      <CNavLink to="/">Main</CNavLink>
-      <CNavLink to="/test">Test</CNavLink>
+
     </div>
   );
 }
