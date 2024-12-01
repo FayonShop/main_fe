b6f3cee3793b195c5cee179db6f8df558254d1c4
Sviatoslav Kytsara <55582196+fayonn@users.noreply.github.com>
2024-12-01 19:20:26 +0200

    Feat/fayfront 28 not found page (#8)
    * fix sidebar styles (remove width)
    
    * add button component, add not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/ui/Button/Button.tsx';
+import styles from './NotFoundPage.module.scss';
+
 export default function NotFoundPage() {
-  return <div>404 Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <p>Ooops...</p>
+      <p>404 Not Found</p>
+      <Button
+        label={'Back to Product page'}
+        onClick={() => navigate('/', { replace: true })}
+        style={{  }}
+      />
+    </div>
+  );
 }
